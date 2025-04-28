80e0abd67c2007a3951e9f5e8c8f43e5d45ecbf3
X-Vitalii <vit.khyzhn@gmail.com>
2025-04-28 22:05:36 +0300

    adjust file structure

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const liList = document.querySelectorAll('.item');
-
-console.log(`Number of categories: ${liList.length}`);
-
-liList.forEach(function (elem) {
-    console.log(`Category: ${elem.children[0].textContent}`);
-    console.log(`Elements: ${elem.children[1].children.length}`);
-});
